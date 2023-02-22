ea1ba15059295faf149f3614b954e84a6e0ae9a1
hectordam06 <dam06.2023@gmail.com>
2023-02-22 10:23:50 +0100

    Proyecto sin documentar

---
+++ src/examen/FechaTest.java
@@ -0,0 +1,23 @@
+package examen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FechaTest {
+
+	@Test
+	void cp1 () {
+		assertfalse(fecha.validarFecha(2,-5,20),false))
+@Test
+void cp2 () {
+	assertequals(fecha.validarFecha (12,24,20),false))
+@Test
+void cp3 () {
+	assertequals(fecha.validarFecha (21,02,20),false))
+@Test
+void cp4 () {
+	assertequals(fechaTest.validarFecha2(31,12,20),false))
+	}
+
+}
